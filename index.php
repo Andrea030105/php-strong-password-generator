1bf2b33c340aa325a081b2b1b3a0e745475c3867
Andrea030105 <tummoloandrea05@gmail.com>
2026-02-12 13:26:36 +0100

    LayOut-done

--- index.php
+++ index.php
@@ -12,8 +12,46 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
-<body>
-
+<body class="bg-secondary">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center my-3 text-light">
+                    Strong Password Generator
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center my-3 fs-2 text-white">
+                    Genera una password sicura
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="container-text bg-info p-3 d-flex align-items-center rounded">
+                    <p class="m-0">
+                        Nessun parametro valido inserito
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="bg-white my-3 p-5 d-flex justify-content-around">
+                    <p>
+                        Lunghezza password:
+                    </p>
+                    <form action="./index.php" method="GET">
+                        <input type="number">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
